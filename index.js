c07e71f9ce345f941d39647ac2dd43f8f19767e0
Alexander Noriega <lambdatoast@gmail.com>
2016-05-10 13:45:39 +0000

    Assemble result into a more sensible structure.

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var fs = require('fs');
 var check = require('./lib/check');
 
-var t0 = '(function () {\n function Foo(a, b, c) {}\nFoo.$inject = ["a", "b"]\n }())';
+var t0 = '(function () {\n Bar.$inject = ["x"]\nfunction Bar(x) {} function Foo(a, b, c) {}\nFoo.$inject = ["a", "b"]\n }())';
 //var t0 = 'function Foo(a, b, c) {}\nif (1) { 1 + 1} else { Foo.$inject = ["a", "b"] }';
 
 var r = check(t0);
 
-/*
-console.log(JSON.stringify(r));
-*/
+//console.log(JSON.stringify(r));
 
 var files = process.argv.slice(2);
 files.forEach(function (val, index, array) {
   //console.log(index + ': ' + val);
 	fs.readFile(val, 'utf8', function(err, contents) {
 		var r = check(contents);
-		if (r.ngInjects.length > 0) {
+		//console.log('r', r);
+		if (r.length > 0) {
 			console.log(JSON.stringify(r), '\n');
 		}
 	});
 });
+/*
+*/
